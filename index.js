3d82a7a0bf4e3c0fbc8040ff24dd8b26944294a2
jameseclement <jameseclement@gmail.com>
2018-12-19 17:17:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ var dodger = document.getElementById('dodger')
 function moveDodgerLeft(){
   var leftNumbers = dodger.style.left.replace('px','')
     var left = parseInt(leftNumbers, 10)
-    dodger.style.left = `${left-1}px`
-    if (left>0) {dodger.style.left
-}
+    if (left > 0) dodger.style.left = `${left-1}px`
+    }
 
 document.addEventListener('keydown', function(e){
   if (e.which === 37){
