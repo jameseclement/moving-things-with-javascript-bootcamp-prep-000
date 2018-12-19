577507d20a09315f204e088f448bb345b0508b09
jameseclement <jameseclement@gmail.com>
2018-12-19 17:16:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const app = "I don't do much."
 
 var dodger = document.getElementById('dodger')
-document.addEventListener('keydown', function(e){
-  if (e.which === 37){
-    var leftNumbers = dodger.style.left.replace('px','')
+
+function moveDodgerLeft(){
+  var leftNumbers = dodger.style.left.replace('px','')
     var left = parseInt(leftNumbers, 10)
     dodger.style.left = `${left-1}px`
+}
+
+document.addEventListener('keydown', function(e){
+  if (e.which === 37){
+    moveDodgerLeft()
+    
     
   }
   })
