146ff969a33422ea80fdc2dce0277a258085fcf3
jameseclement <jameseclement@gmail.com>
2018-12-19 17:08:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much."
+
+document.addEventListener('keydown', function(e){
+  if (e.which === 37){
+    var leftnumbers = dodger.style.left.replace('px','')
+    var left = parsInt(leftNumbers, 10)
+    doger.style.left = `${left-1}px`
+    
+  }
+  })
