79e5c8ee9c5c04d809b0c8ad155b62d8c8a6c03e
jameseclement <jameseclement@gmail.com>
2018-12-19 17:11:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much."
-
+var dodger = document.getElementById('dodger')
 document.addEventListener('keydown', function(e){
   if (e.which === 37){
     var leftNumbers = dodger.style.left.replace('px','')
